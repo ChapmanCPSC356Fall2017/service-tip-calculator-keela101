0959450e2018ac0afabc9b7fed31eb4a2333e146
keela101 <keela101@mail.chapman.edu>
2017-09-13 14:42:50 -0700

    Added restriction of calculated value to 2 dec. places

--- TipCalculator/app/src/main/java/com/chapman/seankeelan/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/chapman/seankeelan/tipcalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.RatingBar;
 import android.widget.Toast;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,8 +75,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcTip(View view) {
         // String finalAmount = Double.toString(tip());
-        this.tip = Math.round(getTip() * 100.0) / 100.0;
-        tipAmount.setText("You should tip: $" + tip + ". Score: " + getScore());
+        DecimalFormat numberFormat = new DecimalFormat("0.00"); // Set decimal limit to two spaces
+        this.tip = Double.valueOf(numberFormat.format(getTip()));
+        tipAmount.setText("You should tip: $" + tip);
+        /* This method sets the text in the TextView widget w/ the id tipAmount to a given String,
+        which cocantenates with the variable 'tip'.
+        NOTE: If the tip is equal to a value in which the digit in the second decimal place is equal
+        to 0, the 0 digit will NOT be displayed. */
     }
 
 }
